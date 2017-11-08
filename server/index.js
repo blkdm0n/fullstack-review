a97024682ba5aeb71b2d5dd966826335ac294f14
blkdm0n <bernard.kitchens@gmail.com>
2017-11-08 13:59:50 -0800

    get/post routes work - rendering fake data

--- server/index.js
+++ server/index.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const fakeData = require('../data');
+const config = require('../config');
+
 let app = express();
+
+
+//OWNER NAME: console.log(fakeData[0].owner.login);
 
 app.use(express.static(__dirname + '/../client/dist'));
 
@@ -8,11 +14,20 @@ app.post('/repos', function (req, res) {
   // This route should take the github username provided
   // and get the repo information from the github API, then
   // save the repo information in the database
+
+  //post request to GH
+  
+  
+  res.send('Hello from POST!')
 });
 
 app.get('/repos', function (req, res) {
   // TODO - your code here!
   // This route should send back the top 25 repos
+  
+
+  //required and rendered fakeData on get request
+  res.send(fakeData);
 });
 
 let port = 1128;
